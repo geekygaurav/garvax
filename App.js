9c3acd153994d22f2f40b987047aae6b7301b32c
Gourav Roy Choudhury <grcnation95@gmail.com>
2020-05-17 20:43:52 +0530

    Add files via upload
    Updated

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Navigation from './App/Navigation/Navigation'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import reducer from './App/Redux/Reducer/index'
 import rootsaga from './App/Saga/index'
-
+import { SafeAreaView } from 'react-navigation'
+import { StatusBar } from 'react-native'
+import {Colors } from './App/Metrics/index'
 /**
  * We are initializing our saga and redux store in this file.
  * Saga middleware will be listneing whenever we invoke any action.
@@ -24,8 +26,14 @@ sagaMiddleware.run(rootsaga)
 export default class App extends Component {
   render() {
     return (
-      <Provider store={ store }>
-        <Navigation/>
+      <Provider store={store}>
+        <SafeAreaView forceInset={{ bottom: 'never' }} style={{ flex: 1 }}>
+          {/* 
+           Customize your own StatusBar here
+          */}
+          <StatusBar backgroundColor={Colors.coffee}/>
+          <Navigation />
+        </SafeAreaView>
       </Provider>
     );
   }
